e528be3b2ad4d0ff177d8f29a8c089c703174b4d
MinhDV <drminh2807@gmail.com>
2024-01-09 21:39:33 +0700

    Upload new build by expo

--- entry-point.js
+++
@@ -1,7 +0,0 @@
-import Constants, { ExecutionEnvironment } from "expo-constants";
-
-const isInExpoMode = Constants.executionEnvironment === ExecutionEnvironment.StoreClient;
-
-const entryPoint = isInExpoMode ? require("./App") : require("./index");
-
-export default entryPoint
